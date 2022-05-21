d53fe96137ac861468c2bb021ecb11225139de75
angst-storm <sergey.kiprin@urfu.me>
2022-05-21 14:00:19 +0500

    правка в маршрутизации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
             },
             {
                 path: '**',
-                redirectTo: '/user/auth',
+                redirectTo: '/control',
                 pathMatch: 'full'
             }
         ]
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { UserService } from '../services/user.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private _userService: UserService) {
+    constructor(private _userService: UserService, private _router: Router) {
     }
 
     public canActivate(
         next: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ): boolean {
-        return this._userService.checkToken();
+        const check: boolean = this._userService.checkToken();
+        if (!check) {
+            this._router.navigate(['/user/auth']);
+        }
+
+        return check;
     }
 }
